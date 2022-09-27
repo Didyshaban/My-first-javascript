f3eeb775d527d628e4c5dba08391f9b3ee98223e
Didyshaban <didyshaban@gmail.com>
2022-09-27 18:42:22 +0200

    my-first-java-learning

---
+++ java.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>My trying session<h2>
+    <p id="deal"></p>
+
+<!-- <p id="demo"></p> -->
+<!-- <button onclick="window.print()">Print this page</button> -->
+<button onclick="myfunction()">click here</button>
+<p id="demoli"></p>
+<p id="demoli1"></p>
+
+<script>
+// document.getElementById("demo").innerHTML = 5 + 6;
+// document.write(7+3);
+// window.alert(4+8);
+// alert(7+12);
+
+// let a,b,c;
+// a=5;
+// b=6;
+// c=a+b;
+// document.getElementById("demo").innerHTML=c;
+
+// document.getElementById("deal").innerHTML="hello Divin";
+
+function myfunction(){
+document.getElementById("demoli").innerHTML="hello";
+document.getElementById("demoli1").innerHTML="world";
+}
+
+
+</script>
+
+</body>
+</html> 
